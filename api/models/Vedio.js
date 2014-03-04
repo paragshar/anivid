778e0f0cd1cf74a98950142297af50c15dddd3cf
DeepanshuKumarSingh <deepanshukumarsingh@gmail.com>
2014-03-04 15:22:15 +0530

    Api for Anivid Vedios

---
+++ api/models/Vedio.js
@@ -0,0 +1,18 @@
+/**
+ * Vedio
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	
+  	email: 'string',
+  	vedio : 'string'
+  }
+
+};
